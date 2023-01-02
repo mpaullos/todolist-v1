ad44e497938ad9f03f5709290a7817f74c07a91f
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-01-02 18:07:04 -0300

    create template base server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+app.listen(port, function() {
+    console.log(`Server running on ${port}`);
+})

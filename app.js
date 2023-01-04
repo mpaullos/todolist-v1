a30adc16157af696eafbbc158f7c5494f9145529
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-01-03 23:43:11 -0300

    day names function

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const express = require("express");
-
-const app = express();
-
 const bodyParser = require("body-parser");
 
-const port = 3000;
+const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    var today = new Date();
+    var currentDay = today.getDay();
+   switch (currentDay) {
+    case 0:
+        day = "Domingo"
+        break;
+    case 1:
+        day = "Segunda"
+        break;
+    case 2:
+        day = "Terça"
+        break;
+    case 3:
+        day = "Quarta"
+        break;
+    case 4:
+        day = "Quinta"
+        break;
+    case 5:
+        day = "Sexta"
+        break;
+    case 6:
+        day = "Sábado"
+        break;
+    
+   
+    default:
+       console.log("CurrentDay is equal to: " + currentDay)
+   }
+    
+    res.render("list", {kindOfDay: day});
+   
 })
 
 
-app.listen(port, function() {
-    console.log(`Server running on ${port}`);
+app.listen(3000, function() {
+    console.log(`Server running on 3000`);
 })

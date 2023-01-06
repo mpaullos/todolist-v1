919771981c76c968bef002c0bfaf8e0c290b833c
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-01-05 22:18:26 -0300

    refactor code and some changes

--- app.js
+++ app.js
@@ -2,47 +2,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Comprar Comida", "Comer Comida", "Cozinhar Comida"];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-   switch (currentDay) {
-    case 0:
-        day = "Domingo"
-        break;
-    case 1:
-        day = "Segunda"
-        break;
-    case 2:
-        day = "Terça"
-        break;
-    case 3:
-        day = "Quarta"
-        break;
-    case 4:
-        day = "Quinta"
-        break;
-    case 5:
-        day = "Sexta"
-        break;
-    case 6:
-        day = "Sábado"
-        break;
-    
-   
-    default:
-       console.log("CurrentDay is equal to: " + currentDay)
-   }
-    
-    res.render("list", {kindOfDay: day});
-   
-})
+app.get("/", function (req, res) {
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("pt-br", options);
+
+  var peguei = req.body.testes;
+  console.log(peguei);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
+});
 
 
-app.listen(3000, function() {
-    console.log(`Server running on 3000`);
-})
+app.listen(3000, function () {
+  console.log(`Server running on 3000`);
+});

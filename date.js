1cbc88830989131d6d5ad583758e935e94980cc1
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-01-09 10:49:31 -0300

    create module date.js

---
+++ date.js
@@ -0,0 +1,24 @@
+exports.getDay = function() {
+
+    const today = new Date();
+      const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+      };
+    
+      const day = today.toLocaleDateString("pt-br", options);
+      return day
+}
+
+exports.captlizeFirstWord = function(word) {
+    const splitAt = word.split(" ");
+    for (var i = 0; i < splitAt.length; i++) {
+      if (splitAt[i] != "de") {
+        splitAt[i] = splitAt[i].charAt(0).toUpperCase() + splitAt[i].slice(1);
+      }
+    }
+    const finalText = splitAt.join(" ");
+    return finalText;
+  }
+  

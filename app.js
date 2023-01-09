ac0c41381c01445d17b212b8211514d128917e93
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-01-09 11:12:44 -0300

    refactor code and import module date js create by me

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-let items = ["Comprar Comida", "Comer Comida", "Cozinhar Comida"];
-let workItems = [];
+const items = ["Comprar Comida", "Comer Comida", "Cozinhar Comida"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,31 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
+  const day = date.capitalizeFirstWord(date.getDate())
 
-  let day = today.toLocaleDateString("pt-br", options);
-  function captlizeFirstWord(word) {
-    var splitAt = word.split(" ");
-    for (var i = 0; i < splitAt.length; i++) {
-      if (splitAt[i] != "de") {
-        splitAt[i] = splitAt[i].charAt(0).toUpperCase() + splitAt[i].slice(1);
-      }
-    }
-    var finalText = splitAt.join(" ");
-    return finalText;
-  }
-  let final = captlizeFirstWord(day);
-
-  res.render("list", { listTitle: final, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work List") {
     workItems.push(item);
     res.redirect("/work");
@@ -50,9 +33,9 @@ app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
   res.render("about");
-})
+});
 
 app.listen(3000, function () {
   console.log(`Server running on 3000`);
